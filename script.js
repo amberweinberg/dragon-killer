00d34f58680e6435a15f278aa1c16062965cad1d
Amber Weinberg <amber@studiosimpati.co>
2024-05-01 09:37:50 -0500

    attack!

--- script.js
+++ script.js
@@ -57,6 +57,12 @@ const locations = [
 	"button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
 	"button functions": [fightSlime, fightBeast, goTown],
 	text: "You enter the cave. You see some monsters."
+  },
+  {
+	name: "fight",
+	"button text": ["Attack", "Dodge", "Run"],
+	"button functions": [attack, dodge, goTown],
+	text: "You are fighting a monster."
   }
 ];
 
@@ -146,5 +152,38 @@ function fightDragon() {
 }
 
 function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+	lose();
+  } else if (monsterHealth <= 0) {
+	defeatMonster();
+  }
+}
+
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+}
+
+function lose() {
 
 }
